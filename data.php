cc00e71ede3776134c2bc7bb0b3cc783d334838d
Rutuja3806 <rutu.jagtap555@gmail.com>
2020-07-20 20:19:18 +0530

    new

---
+++ data.php
@@ -0,0 +1,23 @@
+<?php
+include 'contactus.html';
+include 'connection.php';
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$emailid = $_POST['emailid'];
+$contactno = $_POST['contactno'];
+$address = $_POST['address'];
+
+$sql = "INSERT INTO `contact_us` (`name`,`subject`,`message`,`emailid`,`contactno`,`address`) VALUES('$name','$subject','$message','$emailid','$contactno','$address')";
+
+$result = mysqli_query($conn,$sql);
+  
+if ($result== TRUE){
+    echo ( "<script type='text/javascript'>alert('Thank You for Contacting Us..');
+    windows.history.log(-1);
+    
+     </script>");}
+
+
+
+?>
